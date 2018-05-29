050f73a9659dbbbff117d6c931749ff935c79991
Abel Salgado Romero <abelromero@gmail.com>
2018-05-29 07:58:00 +0200

    initial template

---
+++ src/main/java/io/github/kata/mars/User.java
@@ -0,0 +1,18 @@
+package io.github.kata.mars;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class User {
+
+    private String name;
+    private final LocalDate birthDate;
+
+    public int getAge() {
+        return LocalDate.now().getYear() - birthDate.getYear();
+    }
+}
